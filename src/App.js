5877aaece9396ac58fbf8c1442818b3447d4e84f
FaizanM <faizan.mansur87@gmail.com>
2020-12-07 20:53:30 +0500

    Added theme of covidand added DisplayMessage.js component inside layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import InfoPanel from './components/layout/InfoPanel';
 import Navbar from './components/NavBar.js';
+import DisplayMessage from './components/layout/DisplayMessage';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme= createMuiTheme({
+  palette: {
+    primary: {
+      main: '#b33036',
+    },
+    secondary: {
+      main: '#2f2f2f',
+    },
+  },
+})
+
 
 function App() {
   return (
     <div className="App">
+    <ThemeProvider theme={theme}>
      <Navbar></Navbar>
+     <div>
+       <DisplayMessage></DisplayMessage>
+       <InfoPanel></InfoPanel>
+     </div>
+    </ThemeProvider>
     </div>
   );
 }
---
+++ src/components/layout/DisplayMessage.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+// import background from './coronabkd.png'
+import corona from './coronawobkd.png'
+
+const useStyles = makeStyles({
+  root:
+  {
+    color: 'white',
+    fontWeight: 700,
+    // fontSize: '2rem',
+    fontSize:'2vw',
+    margin: '30px',
+    padding: '30px',
+    wordSpacing: 10,
+    backgroundColor: '#2f2f2f',
+    // backgroundImage: `url(${background})`
+  }
+  ,
+});
+
+
+// const theme = createMuiTheme({
+//   typography: {
+//     fontFamily: [
+//       '-apple-system',
+//       'BlinkMacSystemFont',
+//       '"Segoe UI"',
+//       'Roboto',
+//       '"Helvetica Neue"',
+//       'Arial',
+//       'sans-serif',
+//       '"Apple Color Emoji"',
+//       '"Segoe UI Emoji"',
+//       '"Segoe UI Symbol"',
+//     ].join(','),
+
+//   },
+// });
+
+export default function DisplayMessage() {
+  const classes = useStyles();
+
+  return (
+
+    <div  className={classes.root}>
+      <Grid container >
+        <Grid item xs={12} md={3}>
+          <img src= {corona} style={{width:'100%'}} alt='corona pic'></img>
+        </Grid>
+        <Grid item xs={12} md={9}>
+          <p >
+            Corona Virus is spreading thick and fast. While Doctors and scientists are putting their lives on the line to fight the virus, unfortunately the common man still thinks its a joke.
+            Even if today we take this virus seriously and adopt the prescribed precautionary measures many lives can be saved.
+            <br></br>I hope following stats about covid virus will open your eyes.
+            </p>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
